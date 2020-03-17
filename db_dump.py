50896f76deda4ac96ec498e0c8595ed2ff3a1949
Ashley Young <asyoung@redhat.com>
2020-03-16 21:46:25 -0400

    db/list generated dynamic log list and URLs now work

--- db_dump.py
+++ db_dump.py
@@ -1,10 +1,6 @@
 #! /usr/bin/env python3
-
 import json
 import pymysql
-
-db = 'dummy_data'
-sql = "select timestamp, rpm, airflow, fronto2, timing, inttemp, cooltemp, knockret, speed, throtpos from " + db+ ";"
 
 def dbconnect(sql):
    host = "10.68.25.61"
@@ -18,13 +14,16 @@ def dbconnect(sql):
    con.close()
    return tables
 
-dump = dbconnect(sql)
-log = {}
-log[db] = []
-for i in dump:
-   time = i['timestamp']
-   log[db].append({'time': i['timestamp'], 'rpm': i['rpm'], 'airflow': float(i['airflow']), 'fronto2': float(i['fronto2']), 'timing': float(i['timing']), 'inttemp': float(i['inttemp']), 'cooltemp': float(i['cooltemp']), 'knockret': float(i['knockret']), 'speed': float(i['speed']), 'throtpos': float(i['throtpos'])})
+def log_api(log_table):
+   db = log_table
+   sql = "select timestamp, rpm, airflow, fronto2, timing, inttemp, cooltemp, knockret, speed, throtpos from " + db + ";"
+   dump = dbconnect(sql)
+   log = {}
+   log[db] = []
+   for i in dump:
+      time = i['timestamp']
+      log[db].append({'time': i['timestamp'], 'rpm': i['rpm'], 'airflow': float(i['airflow']), 'fronto2': float(i['fronto2']), 'timing': float(i['timing']), 'inttemp': float(i['inttemp']), 'cooltemp': float(i['cooltemp']), 'knockret': float(i['knockret']), 'speed': float(i['speed']), 'throtpos': float(i['throtpos'])})
 
-jlog = json.dumps(log, indent=4)
+   jlog = json.dumps(log, indent=4)
 
-print(jlog)
+   print(jlog)
--- main.py
+++ main.py
@@ -1,42 +1,27 @@
 from flask import Flask
 from flask import render_template
+import db_dump as dump
 
 import pymysql
 
 app = Flask(__name__)
 
-def dbconnect():
-   host = "10.68.25.61"
-   user = "dsm_user"
-   password = "password"
-   db = "dsmutils_csv"
-   con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.DictCursor)
-   cur = con.cursor()
-   cur.execute("SHOW TABLES FROM dsmutils_csv;")
-   tables = cur.fetchall()
-   con.close()
-   return tables
+#db = 'dummy_data'
+#sql = "select timestamp, rpm, airflow, fronto2, timing, inttemp, cooltemp, knockret, speed, throtpos from " + db+ ";"
 
 some_name = 'Ashley'
 
-@app.route('/logs/lpath')
-def log_tables(lpath):
-    return render_template("basic.html")
-
 @app.route("/")
 def index():
-  results = dbconnect()
-  return render_template('base.html', table=results)
+   sql= "SHOW TABLES FROM dsmutils_csv;"
+   results = dump.dbconnect(sql)
+   return render_template('base.html', table=results)
 
-lpath = dbconnect()
-
-for row in lpath:
-  npath = row['Tables_in_dsmutils_csv']
-
-  @app.route('/logs/' + npath)
-  def logs():
-    results = dbconnect()
-    return render_template('base.html', table=results)
+@app.route('/logs/<lpath>')
+def log_tables(lpath):
+   sql= "SHOW TABLES FROM dsmutils_csv;"
+   results = dump.dbconnect(sql)
+   return render_template("base.html", table=results)
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=4000)
+   app.run(host='0.0.0.0', port=4000, use_reloader=True)
